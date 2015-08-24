9320e36a12b92ef41ff22cb62158f762ee80bc8e
Daiwei <daiweilu123@gmail.com>
2015-08-24 17:43:39 -0400

    Better communication

--- bin/wpg.js
+++ bin/wpg.js
@@ -1,12 +1,23 @@
 #!/usr/bin/env node
 
-var Bluebird = require('bluebird');
 var program = require('commander');
-var path = require('path');
-var _ = require('lodash');
 var package = require('../package.json');
+
+program
+  .version(package.version)
+  .description('WebPlayground bootstraps simple HTML, JS, CSS files' +
+    'and setup live-reloading server for you.')
+  .parse(process.argv);
+
+// Defer requires for better performance on outputing helps
+
+var Bluebird = require('bluebird');
+var path = require('path');
 var fs = require('../lib/promisified/fs');
-var server = require('../index');
+var Log = require('../lib/util/log');
+var chalk = require('chalk');
+
+var info = Log.info;
 
 function existsAsync(file) {
   return Bluebird.fromNode(function (callback) {
@@ -33,29 +44,45 @@ function creatEmpty(dest) {
   });
 }
 
-program
-  .version(package.version)
-  .parse(process.argv);
+function deferLog(str) {
+  return function () {
+    info(str);
+  };
+}
+
+function logFileCreation(file) {
+  return deferLog('Created ' + file);
+}
 
 existsAsync(path.resolve('playground.yml'))
   .then(function (exists) {
     return exists ? true : existsAsync(path.resolve('playground.json'));
   })
-  .then(function (exists) {
+  .done(function (exists) {
     if (exists) {
-      return [];
-    } else {
-      var p1 = copy(
-        path.join(__dirname, '..', 'template', 'playground.yml'),
-        path.resolve('playground.yml'));
-      var p2 = copy(
-        path.join(__dirname, '..', 'template', 'html.html'),
-        path.resolve('html.html'));
-      var p3 = creatEmpty(path.resolve('css.css'));
-      var p4 = creatEmpty(path.resolve('javascript.js'));
-
-      return [p1, p2, p3, p4];
+      require('../index').start();
+      return;
     }
-  })
-  .all()
-  .done(server.start);
+
+    info('Did not find playground file, scaffold files for you.');
+
+    var p1 = copy(
+      path.join(__dirname, '..', 'template', 'playground.yml'),
+      path.resolve('playground.yml'))
+      .tap(logFileCreation('playground.yml'));
+
+    var p2 = copy(
+      path.join(__dirname, '..', 'template', 'html.html'),
+      path.resolve('html.html'))
+      .tap(logFileCreation('html.html'));
+
+    var p3 = creatEmpty(path.resolve('css.css'))
+      .tap(logFileCreation('css.css'));
+
+    var p4 = creatEmpty(path.resolve('javascript.js'))
+      .tap(logFileCreation('javascript.js'));
+
+    Bluebird
+      .all([p1, p2, p3, p4])
+      .done(deferLog('Run ' + chalk.green('wpg') + ' to start live-reload server'));
+  });
