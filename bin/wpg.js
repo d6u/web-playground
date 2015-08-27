054cc9dc75fb212cd8bbe0e4592e146cfdfc128e
Daiwei <daiweilu123@gmail.com>
2015-08-26 22:44:44 -0400

    Refactor

--- bin/wpg.js
+++ bin/wpg.js
@@ -5,84 +5,47 @@ var package = require('../package.json');
 
 program
   .version(package.version)
-  .description('WebPlayground bootstraps simple HTML, JS, CSS files' +
-    'and setup live-reloading server for you.')
+  .description(package.description)
   .parse(process.argv);
 
 // Defer requires for better performance on outputing helps
 
-var Bluebird = require('bluebird');
-var path = require('path');
-var fs = require('../lib/promisified/fs');
-var Log = require('../lib/util/log');
-var chalk = require('chalk');
+var Bluebird      = require('../lib/promise/bluebird-mod');
+var path          = require('path');
+var chalk         = require('chalk');
+var _             = require('lodash');
+var Log           = require('../lib/util/log');
+var readFileAsync = require('../lib/fs-util/read-file-async');
+var copy          = require('../lib/fs-util/copy');
+var creatEmpty    = require('../lib/fs-util/create-empty');
 
 var info = Log.info;
 
-function existsAsync(file) {
-  return Bluebird.fromNode(function (callback) {
-    fs.exists(file, function (exists) {
-      callback(null, exists);
-    });
-  });
-}
-
-function copy(fromFile, toFile) {
-  return Bluebird.fromNode(function (callback) {
-    fs.createReadStream(fromFile)
-      .pipe(fs.createWriteStream(toFile))
-      .on('finish', callback);
-  });
-}
-
-function creatEmpty(dest) {
-  return Bluebird.fromNode(function (callback) {
-    var stream = fs.createWriteStream(dest);
-    stream.on('finish', callback);
-    stream.write('');
-    stream.end();
-  });
-}
-
-function deferLog(str) {
-  return function () {
-    info(str);
-  };
-}
-
 function logFileCreation(file) {
-  return deferLog('Created ' + file);
+  var log = _.flow(chalk.green, function (str) { return 'Created ' + str; }, info);
+  return _.ary(_.partial(log, file), 0);
 }
 
-existsAsync(path.resolve('playground.yml'))
-  .then(function (exists) {
-    return exists ? true : existsAsync(path.resolve('playground.json'));
+Bluebird
+  .resolve(['playground.yml', 'playground.json'])
+  .tryInOrder(readFileAsync)
+  .then(function () {
+    require('../index').start();
   })
-  .done(function (exists) {
-    if (exists) {
-      require('../index').start();
-      return;
-    }
-
+  .catch(function (err) {
+    console.log(err);
     info('Did not find playground file, scaffold files for you.');
 
-    var p1 = copy(
-      path.join(__dirname, '..', 'template', 'playground.yml'),
-      path.resolve('playground.yml'))
+    var p1 = copy(path.join(__dirname, '..', 'template', 'playground.yml'), 'playground.yml')
       .tap(logFileCreation('playground.yml'));
 
-    var p2 = copy(
-      path.join(__dirname, '..', 'template', 'html.html'),
-      path.resolve('html.html'))
+    var p2 = copy(path.join(__dirname, '..', 'template', 'html.html'), 'html.html')
       .tap(logFileCreation('html.html'));
 
-    var p3 = creatEmpty(path.resolve('css.css'))
-      .tap(logFileCreation('css.css'));
-
-    var p4 = creatEmpty(path.resolve('javascript.js'))
-      .tap(logFileCreation('javascript.js'));
+    var p3 = creatEmpty('css.css').tap(logFileCreation('css.css'));
+    var p4 = creatEmpty('javascript.js').tap(logFileCreation('javascript.js'));
 
     Bluebird
-      .all([p1, p2, p3, p4])
-      .done(deferLog('Run ' + chalk.green('wpg') + ' to start live-reload server'));
+      .join(p1, p2, p3, p4)
+      .done(_.partial(info, 'Run ' + chalk.green('wpg') + ' to start live-reload server'));
   });
