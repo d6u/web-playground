90a6c82bf989e82345ed2c5751a6e7183035863b
Daiwei <daiweilu123@gmail.com>
2015-09-27 00:54:21 -0400

    Clean up

--- bin/wpg.js
+++ bin/wpg.js
@@ -10,26 +10,4 @@ program
   .description(packageJson.description)
   .parse(process.argv);
 
-require('../index')();
-
-// var chalk         = require('chalk');
-// var creatEmpty    = require('../lib/fs-util/create-empty');
-
-// Bluebird
-//   .resolve(['playground.yml', 'playground.json'])
-//   .tryInOrder(readFileAsync)
-//   .catch(function (err) {
-
-//     var p2 = copy(path.join(__dirname, '..', 'template', 'html.html'), 'html.html')
-//       .tap(logFileCreation('html.html'));
-
-//     var p3 = creatEmpty('css.css').tap(logFileCreation('css.css'));
-//     var p4 = creatEmpty('javascript.js').tap(logFileCreation('javascript.js'));
-
-//     Bluebird
-//       .join(p1, p2, p3, p4)
-//       .done(_.partial(info, 'Run ' + chalk.green('wpg') + ' to start live-reload server'));
-//   })
-//   .then(function () {
-//     require('../index').start();
-//   });
+require('../lib')();
