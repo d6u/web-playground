a8bdd1d44593657f5daf45812e64212c25f74dc5
Daiwei <daiweilu123@gmail.com>
2015-10-11 21:19:46 -0400

    No open option

--- bin/wpg.js
+++ bin/wpg.js
@@ -9,8 +9,12 @@ program
   .version(pkg.version)
   .description(pkg.description)
   .option('-b, --bundle', 'bundle you playground into a single HTML file, which can be uploaded to the Internet')
+  .option('--no-open-browser', 'do not auto open a new browser window when start')
+  .option('--no-live-reload', 'disable live reloading (auto refresh browser when file changes)')
   .parse(process.argv);
 
 require('../lib')({
-  bundle: program.bundle
+  bundle: program.bundle,
+  openBrowser: program.openBrowser,
+  liveReload: program.liveReload,
 });
