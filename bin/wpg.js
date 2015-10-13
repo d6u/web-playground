87cec9d26511ee7b06a6b499f4df6641ff8c21ef
Daiwei <daiweilu123@gmail.com>
2015-10-12 22:05:59 -0400

    Refactor preStart

--- bin/wpg.js
+++ bin/wpg.js
@@ -9,12 +9,14 @@ program
   .version(pkg.version)
   .description(pkg.description)
   .option('-b, --bundle', 'bundle you playground into a single HTML file, which can be uploaded to the Internet')
+  .option('-d, --target-dir <dir-name>', 'target different directory')
   .option('--no-open-browser', 'do not auto open a new browser window when start')
   .option('--no-live-reload', 'disable live reloading (auto refresh browser when file changes)')
   .parse(process.argv);
 
 require('../lib')({
   bundle: program.bundle,
+  targetDir: program.targetDir,
   openBrowser: program.openBrowser,
   liveReload: program.liveReload,
 });
