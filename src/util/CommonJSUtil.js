52506c3a5ba183059f798749e8cfa60a07a4648c
Daiwei <daiweilu123@gmail.com>
2016-01-10 15:05:54 -0500

    Apply babel loader

--- src/util/CommonJSUtil.js
+++ src/util/CommonJSUtil.js
@@ -5,6 +5,26 @@ import { Observable } from 'rx';
 import { fromCallback } from 'bluebird';
 import { RenderError } from '../Error';
 
+function createOptions(targetDir) {
+  return {
+    entry: [join(targetDir, 'js.js')],
+    output: {
+      path: targetDir,
+      filename: 'bundle.js',
+    },
+    loaders: [
+      {
+        test: /\.(js|es6|es|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react', 'stage-0']
+        }
+      }
+    ]
+  };
+}
+
 /**
  * Bundle JS file with CommonJS module, return the bundled JS file as string
  *
@@ -16,13 +36,7 @@ export function createBundle(targetDir) {
   return fromCallback((done) => {
     const fs = new MemoryFS();
 
-    const compiler = webpack({
-      entry: [join(targetDir, 'js.js')],
-      output: {
-        path: targetDir,
-        filename: 'bundle.js',
-      }
-    });
+    const compiler = webpack(createOptions(targetDir));
 
     compiler.outputFileSystem = fs;
 
@@ -49,13 +63,7 @@ export function createBundlerStream(targetDir) {
   return Observable.create((observer) => {
     const fs = new MemoryFS();
 
-    const compiler = webpack({
-      entry: [join(targetDir, 'js.js')],
-      output: {
-        path: targetDir,
-        filename: 'bundle.js',
-      }
-    });
+    const compiler = webpack(createOptions(targetDir));
 
     compiler.outputFileSystem = fs;
 
